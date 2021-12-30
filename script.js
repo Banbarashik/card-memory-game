d90121e83aa57f6d90ab1f3efccb573357809f73
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-31 04:38:26 +0700

    generate classes for pairs of cards

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 // DOM elements into variables
 const gameField = document.getElementById('game--field');
+const cards = document.getElementsByClassName('card');
 
 const setGridCells = function (columns, rows) {
   gameField.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
@@ -13,6 +14,23 @@ const fillGridCells = function (columns, rows) {
   for (let i = 0; i < columns * rows; i++) {
     const cell = document.createElement('div');
 
+    cell.classList.add('card');
+
     gameField.appendChild(cell);
   }
 };
+
+setGridCells(6, 6);
+
+// Connecting pairs of cards by 'pair-i' class
+let i = 1;
+Array.from(cards).forEach((card, index) => {
+  if (index % 2 === 0) {
+    card.classList.add(`pair-${i}`);
+    i++;
+  } else {
+    card.classList.add(`pair-${i - 1}`);
+  }
+});
+
+console.log(cards);
