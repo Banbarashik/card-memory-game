447f1b62c65a910c1294570f851dae606110b17e
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-31 12:21:28 +0700

    randomly place cards in the grid

--- script.js
+++ script.js
@@ -18,19 +18,32 @@ const fillGridCells = function (columns, rows) {
 
     gameField.appendChild(cell);
   }
+
+  setCardsPairClasses();
+  placeCardsRandomly(columns, rows);
+};
+
+// Connecting pairs of cards by a 'pair-i' class
+const setCardsPairClasses = function () {
+  let i = 1;
+  Array.from(cards).forEach((card, index) => {
+    if (index % 2 === 0) {
+      card.classList.add(`pair-${i}`);
+      i++;
+    } else {
+      card.classList.add(`pair-${i - 1}`);
+    }
+  });
+};
+
+// randomly place cards in the grid
+const placeCardsRandomly = function (columns, rows) {
+  Array.from(cards).forEach(card => {
+    card.style.gridColumnStart = Math.floor(Math.random() * columns) + 1;
+    card.style.gridRowStart = Math.floor(Math.random() * rows) + 1;
+  });
 };
 
 setGridCells(6, 6);
-
-// Connecting pairs of cards by 'pair-i' class
-let i = 1;
-Array.from(cards).forEach((card, index) => {
-  if (index % 2 === 0) {
-    card.classList.add(`pair-${i}`);
-    i++;
-  } else {
-    card.classList.add(`pair-${i - 1}`);
-  }
-});
 
 console.log(cards);
